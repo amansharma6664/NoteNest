b2314b75c5a8bb288f57f800f8b4836cd9e8b1d8
Aman Kumar Sharma <amankumarsharma01234@gmail.com>
2025-08-08 16:51:36 +0530

    Added Readme

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
     e.preventDefault(); // Prevents default form reload
 
     // Send POST request to backend login API
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+    const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/auth/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(credentials),
